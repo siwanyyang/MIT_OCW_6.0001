2f1062564793f285fb56001efa281bfd49e21432
Yangsiwan <swy12190@gmail.com>
2021-12-11 17:49:50 -0800

    pset01

--- psets/ps0/ps0.py
+++ psets/ps0/ps0.py
@@ -1,7 +1,7 @@
-"""
+
 import numpy as np
 
-x = int(input("Enter the number x:"))
-y = int(input("Enter the number y:"))
+x = int(input("Enter the number x: "))
+y = int(input("Enter the number y: "))
 print("X**y = ", x ** y)
 print("log(x) = ",np.log2(x))
